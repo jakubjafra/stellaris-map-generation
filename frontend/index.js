ea48afb5d33353c3c0184923d577f4763fdc45c4
Jakub Jafra <jakub.jafra@gmail.com>
2017-05-04 18:19:16 +0800

    structure

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express();
 const fs = require('fs');
+
+const app = express();
 
 app.get(/\/api\/(.+)/, function (req, res) {
     const sampleName = req.params['0'];
@@ -9,13 +10,27 @@ app.get(/\/api\/(.+)/, function (req, res) {
         res
             .set('Content-Type', 'application/json')
             .status(200)
-            .send(fs.readFileSync(`./samples/${sampleName}`).toString());
+            .send(fs.readFileSync(`../backend/samples/${sampleName}`).toString());
     } catch(e){
         res.status(404).send('Not found.');
     }
 });
 
-app.use(express.static('frontend/public'));
+app.get('/', function(req, res){
+    res
+        .set('Content-Type', 'text/html')
+        .status(200)
+        .send(fs.readFileSync(`./public/templates/index.html`).toString());
+});
+
+app.get('/:sampleName', function(req, res){
+    res
+        .set('Content-Type', 'text/html')
+        .status(200)
+        .send(fs.readFileSync(`./public/templates/stats.html`).toString());
+});
+
+app.use('/assets', express.static('./assets'));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
